f54f4644804371f50da8151b9dcb7c36374f9ce9
maruf <mdmarufbinsalim@gmail.com>
2026-05-04 18:39:17 +0600

    feat: added resource logic for users with plan

--- src/modules/parent-resources/parent-resources.module.ts
+++ src/modules/parent-resources/parent-resources.module.ts
@@ -3,11 +3,9 @@ import { TypeOrmModule } from "@nestjs/typeorm";
 import { ParentResourcesController } from "./parent-resources.controller";
 import { ParentResourcesService } from "./parent-resources.service";
 import { UserPlan } from "../subscriptions/entities/user-plan.entity";
-import { Child } from "../children/entities/child.entity";
-import { ChildModule } from "../children/entities/child-module.entity";
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserPlan, Child, ChildModule])],
+  imports: [TypeOrmModule.forFeature([UserPlan])],
   controllers: [ParentResourcesController],
   providers: [ParentResourcesService],
 })
--- src/modules/parent-resources/parent-resources.service.ts
+++ src/modules/parent-resources/parent-resources.service.ts
@@ -1,27 +1,18 @@
 import { Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
-import { In, Repository } from "typeorm";
+import { Repository } from "typeorm";
 import { UserPlan } from "../subscriptions/entities/user-plan.entity";
-import { Child } from "../children/entities/child.entity";
-import { ChildModule } from "../children/entities/child-module.entity";
-import {
-  MODULE_UNLOCK_DAYS,
-  MAX_MODULES,
-} from "../../constants/modules.constants";
 import {
   PARENT_RESOURCES,
   ParentResource,
 } from "../../common/parent-resources.constants";
+import { PlanName } from "../subscriptions/entities/plan.entity";
 
 @Injectable()
 export class ParentResourcesService {
   constructor(
     @InjectRepository(UserPlan)
     private readonly userPlanRepository: Repository<UserPlan>,
-    @InjectRepository(Child)
-    private readonly childRepository: Repository<Child>,
-    @InjectRepository(ChildModule)
-    private readonly childModuleRepository: Repository<ChildModule>,
   ) {}
 
   async getResources(userId: string): Promise<ParentResource[]> {
@@ -29,44 +20,15 @@ export class ParentResourcesService {
       where: { userId, isActive: true },
     });
 
-    const unlockedModules = this.resolveUnlockedModules(userPlan ?? null);
-    if (unlockedModules.size === 0) return [];
+    if (!userPlan) return [];
 
-    const children = await this.childRepository.find({ where: { userId } });
-    if (children.length === 0) return [];
-
-    const childIds = children.map((c) => c.id);
-    const completedModules = await this.childModuleRepository.find({
-      where: { childId: In(childIds), isCompleted: true },
-    });
-
-    const completedModuleNos = new Set(completedModules.map((m) => m.moduleNo));
-
-    return PARENT_RESOURCES.filter(
-      (r) => unlockedModules.has(r.module) && completedModuleNos.has(r.module),
-    );
-  }
-
-  private resolveUnlockedModules(userPlan: UserPlan | null): Set<number> {
-    const unlocked = new Set<number>();
-
-    for (let moduleNo = 1; moduleNo <= MAX_MODULES; moduleNo++) {
-      if (moduleNo === 1) {
-        unlocked.add(1);
-        continue;
-      }
-
-      if (!userPlan?.purchasedAt) continue;
-
-      const delayDays = MODULE_UNLOCK_DAYS[moduleNo] ?? (moduleNo - 2) * 14;
-      const unlockDate = new Date(userPlan.purchasedAt);
-      unlockDate.setDate(unlockDate.getDate() + delayDays);
-
-      if (new Date() >= unlockDate) {
-        unlocked.add(moduleNo);
-      }
+    if (
+      userPlan.plan.name === PlanName.SOLO_EXPLORER ||
+      userPlan.plan.name === PlanName.FAMILY_PACK
+    ) {
+      return PARENT_RESOURCES;
     }
 
-    return unlocked;
+    return [];
   }
 }
