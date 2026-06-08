16b0304b6733c24b559c15ec19bf7e478be8df9a
maruf <mdmarufbinsalim@gmail.com>
2026-06-08 13:07:17 +0600

    feat: updated module-registry.ts

--- src/constants/module-registry.ts
+++ src/constants/module-registry.ts
@@ -45,9 +45,8 @@ const moduleRegistry = {
         "1": { screens: 2 },
         "2": { screens: 3 },
         "3": { screens: 4 },
-        "4": { screens: 1 },
-        "5": { screens: 0 },
-        "6": { screens: 0 },
+        "4": { screens: 2 },
+        "5": { screens: 1 },
       },
     },
     "6": {
