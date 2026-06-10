ed7990f8eae5f94b5c44fd9957c1aa6b94b52ad5
maruf <mdmarufbinsalim@gmail.com>
2026-06-10 11:43:24 +0600

    feat: added dotenv as dependency

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,3 @@
-import "dotenv/config";
 import { NestFactory } from "@nestjs/core";
 import { ValidationPipe } from "@nestjs/common";
 import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
