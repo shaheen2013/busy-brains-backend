b2f55e07108a5f5c6e03d1a87a148aa94667d413
maruf <mdmarufbinsalim@gmail.com>
2026-06-18 13:16:47 +0600

    feat: updated registry

--- src/constants/module-registry.ts
+++ src/constants/module-registry.ts
@@ -52,7 +52,7 @@ const moduleRegistry = {
       quests: {
         "1": { screens: 1 },
         "2": { screens: 1 },
-        "3": { screens: 2 },
+        "3": { screens: 3 },
         "4": { screens: 1 },
         "5": { screens: 2 },
       },
